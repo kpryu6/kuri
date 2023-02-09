17bbb77a72703d0a0612a6b8143c93c4204f6158
Lee_Coderrr <80030418+Lee-Coderrr@users.noreply.github.com>
2023-02-09 20:33:21 +0900

    Update IP address

--- client-go/main.go
+++ client-go/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -11,9 +13,35 @@ import (
 	"k8s.io/client-go/rest"
 )
 
+type Data struct {
+	Token string `json:"token"`
+	Host  string `json:"host"`
+}
+
 func main() {
-	token := "eyJhbGciOiJSUzI1NiIsImtpZCI6IkYzalRWYmgyc2tOTlQ3encybUFsWUhHOXpSeXpVUWlmd3VPSlZpNkZfNXcifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImRlZmF1bHQtdG9rZW4tcGNzeHYiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiZGVmYXVsdCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjBmNDEwMDY2LTYzYzUtNDhhNC04N2E1LTUyNDE0NjgyYmFjYyIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0OmRlZmF1bHQifQ.pkZPEBgPgsSTrwuZOh2bNj2Qi_dg66Uo3XeiZ82p2fqvMdfagDxr3-Kjqz7UAts-oA-BHlNBM5HXsZBPWcARn915At47tjHsZiqB09iH_BvoKXXqEteNPVMxbkpp70BPAmR3BqXWFjOnjtNKqbwhZ66JtclgTG1r_nYtmhvxClzTmkt05bETR9zkobWb4x_c59_VTnG5kBs21zcxr5-ycBhEfUPBzN_Eg6IqBGXbq1xThi9XzWotntM6CHRgYGm3cyMkLh8SFI_BqqCrohpBvhLSu6KSj9r8Shi3luxFdePd1HzjD9wqiJJsLJTnPk72OSDJR4p-C6JG178126k94A"
-	host := "https://10.0.20.124:6443"
+	data := Data{}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		// Get the data from frontend
+		res, err := http.Get("10.96.100.0:80/tokenhost")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer res.Body.Close()
+
+		// Decode the data from JSON to the Data struct
+		err = json.NewDecoder(res.Body).Decode(&data)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Data:", data)
+	})
+
+	host := data.Host
+	token := data.Token
 
 	// Create a config with the given host and token
 	config := &rest.Config{
